6a64be0fb6e48d2cd06ef354ca2ab608c0490e52
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-07-01 20:09:45 +0600

    clerk setup completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <h1 className="text-shadow-blue-400 font-bold text-red-600 text-5xl">
-        Hello React
-      </h1>
-      <Button>Click me</Button>
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </>
   );
 }

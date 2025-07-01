ef617358472b2de9309e0d036b427d4667cade48
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-07-01 19:58:51 +0600

    frontend setup completed with tailwind css and shad cn ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-shadow-blue-400 font-bold text-red-600 text-5xl">
+        Hello React
+      </h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;

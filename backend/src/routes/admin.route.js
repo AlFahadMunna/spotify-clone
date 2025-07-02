30dfad012844eada5ad41a2121905dfbad1583eb
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-07-02 19:22:05 +0600

    delete album

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
-import { createSong, deleteSong } from "../controllers/admin.controller.js";
+import {
+  createSong,
+  deleteSong,
+  createAlbum,
+  deleteAlbum,
+} from "../controllers/admin.controller.js";
 
 const router = Router();
 
 router.post("/songs", protectRoute, requireAdmin, createSong);
 router.delete("/delete/:id", protectRoute, requireAdmin, deleteSong);
+router.post("/albums", protectRoute, requireAdmin, createAlbum);
+router.delete("/albums/:id", protectRoute, requireAdmin, deleteAlbum);
 
 export default router;
